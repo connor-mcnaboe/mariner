63480b7dd5e434a9cafccf285bb1a4fd6bc4d8d0
Connor McNaboe <mcnaboeconnor@gmail.com>
2022-02-26 15:28:20 -0500

    Calculate time on Mars

--- Martian Watch WatchKit Extension/Views/MartianTimeView.swift
+++ Martian Watch WatchKit Extension/Views/MartianTimeView.swift
@@ -8,21 +8,33 @@
 import SwiftUI
 
 struct MartianTimeView: View {
+    let marsColor = Color(red: 237/255, green: 76/255, blue: 42/255)
     @State var martianTime: Double =
     MartianTimeService().calcualteMartianTime(currentEarthTimeSeconds: Date().timeIntervalSince1970, longitude: 0.0)
     let timer = Timer.publish(every: 1, on: .current, in: .common).autoconnect()
     
     var body: some View {
-        Text(MartianTimeService().convertDoubleToDigialtclockTime(mct: martianTime))
-            .onReceive(timer) { input in
-                self.martianTime =
-                MartianTimeService().calcualteMartianTime(currentEarthTimeSeconds: Date().timeIntervalSince1970, longitude: 0.0)
+        ZStack {
+            Circle()
+                .fill(marsColor)
+                .scaledToFit()
+            
+            VStack {
+                Text("Mars Time").font(.subheadline)
+                
+                Text(MartianTimeService().convertDoubleToDigialtclockTime(mct: martianTime))
+                    .onReceive(timer) { input in
+                        self.martianTime =
+                        MartianTimeService().calcualteMartianTime(currentEarthTimeSeconds: Date().timeIntervalSince1970, longitude: 0.0)
+                    }.font(.title)
+                    .padding()
             }
+        }
     }
-}
-
-struct MartianTimeView_Previews: PreviewProvider {
-    static var previews: some View {
-        MartianTimeView()
+    
+    struct MartianTimeView_Previews: PreviewProvider {
+        static var previews: some View {
+            MartianTimeView()
+        }
     }
 }
